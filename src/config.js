40711387f1757196ca3ef25710dd18f010aba381
aaron-wc <likeclem30@gmail.com>
2021-02-01 14:13:20 -0600

    init commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  services: {
+    posts: {
+      uri: "http://post-api-service/api/posts",
+    },
+    tasks: {
+      uri: "http://task-api-service/api/tasks",
+    },
+  },
+}
+
+module.exports = config;
